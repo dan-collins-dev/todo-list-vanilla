37ee4db991037d3407df57e7619e18aa3315f330
Dan Collins <dan.collins.dev@gmail.com>
2024-08-08 16:50:59 -0400

    Added dialog, its related styles, and beginning of adding new todo functionality

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,14 @@ const createTodo = () => {}
 const deleteTodo = () => {}
 
 const updateTodo = () => {}
+
+const addButton = document.querySelector(".header__add-btn");
+
+addButton.addEventListener("click", (e) => {
+    const addModal = document.querySelector(".add-modal");
+    addModal.showModal();
+    // Opens Modal
+    // User Enters required info
+    // User saves todo
+    // todo card created and added to the DOM
+})
