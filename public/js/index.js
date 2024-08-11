a8179ed9a1641e44cae2a65eac2882796319d2e1
Dan Collins <dan.collins.dev@gmail.com>
2024-08-11 14:14:34 -0400

    made form fields required. Added close button functionality.

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,35 @@
 "use strict";
 
-const createTodo = () => {}
+const addBtn = document.querySelector(".header__add-btn");
+const cancelBtn = document.querySelector(".add_modal__cancel_btn");
+const createBtn = document.querySelector(".add_modal__create_btn");
 
-const deleteTodo = () => {}
+const createTodoCard = () => {}
 
-const updateTodo = () => {}
+const deleteTodoCard = () => {}
 
-const addButton = document.querySelector(".header__add-btn");
+const updateTodoCard = () => {}
 
-addButton.addEventListener("click", (e) => {
+
+addBtn.addEventListener("click", e => {
     const addModal = document.querySelector(".add-modal");
     addModal.showModal();
+});
 
-    // User Enters required info
+cancelBtn.addEventListener("click", e => {
+    e.preventDefault();
+    const addModal = document.querySelector(".add-modal");
+    addModal.close();
+})
+
+createBtn.addEventListener("click", e => {
+    // e.preventDefault();
     const name = document.querySelector(".add-modal__form__name-input");
-    // User saves todo
-    // todo card created and added to the DOM
+    const dueDate = document.querySelector(".add-modal__form__due-date-input");
+    const priority = document.querySelector(".add-modal__form__priority-input");
+
+    // Quick check for values and types (remove later)
+    console.log(name.value, typeof name.value);
+    console.log(dueDate.value, typeof dueDate.value)
+    console.log(priority.value, typeof priority.value)
 })
